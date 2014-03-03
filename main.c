2fca8dc5242c579cab88582f512531ec0afdabb7
liobouchan <leo.bouchan@gmail.com>
2014-03-03 17:38:00 -0600

    Insertar al INicio e imprimir listos

--- main.c
+++ main.c
@@ -3,7 +3,8 @@ p = posicionador
 list = lista
 sList = aLista
 next = sigAlumno
-
+node = Alumno
+data = boleta
 */
 
 #include <stdio.h>
@@ -21,13 +22,15 @@ next = sigAlumno
 
 //Funciones Prototipo
   //Inicializar la lista
-    void InitList( lista *aLista );
+    void InicializarLista ( lista *aLista );
   //Agregar Valor al inicio
-    void InsertarAlInicio( lista *aLista , int boleta );
+    void InsertarAlInicio ( lista *aLista , int boleta );
+  //Imprimir Lista
+    void ImprimirLista ( lista *aLista );
 
 //Funciones
   //Inicialisa la lista de structs
-    void InitList( lista *aLista ){
+    void InicializarLista( lista *aLista ){
       aLista -> start = NULL;
     }
   //Insertar Valor al Inicio
@@ -38,7 +41,32 @@ next = sigAlumno
       posicionador -> sigAlumno = aLista -> start;
       aLista -> start = posicionador;
     }
+  //Insertar al Final FALTA FALTA FALTA
+    void InsertarAlFinAL( lista *aLista , int boleta ){
+      nodoAlumno *posicionador;
+      posicionador = malloc( sizeof( nodoAlumno ));
+      posicionador -> boleta = boleta;
+      posicionador -> sigAlumno = aLista -> start;
+      aLista -> star = posicionador;
+    }
+  //Imprimir Lista
+    void ImprimirLista ( lista *aLista ){
+      nodoAlumno *posicionador = aLista -> start;
+      while( posicionador != NULL ){
+        printf("%d\n", posicionador -> boleta);
+        posicionador = posicionador -> sigAlumno;
+      }
+    }
 
 int main(){
 
+  int x;
+  lista MiLista;
+  InicializarLista( &MiLista );
+
+  for (x = 0; x < 6; ++x){
+    InsertarAlInicio(&MiLista, x);
+  }
+
+  ImprimirLista(&MiLista); 
 }
