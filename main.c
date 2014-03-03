566ef1f232eaae73d592c909799455383e6656b1
liobouchan <leo.bouchan@gmail.com>
2014-03-03 16:23:59 -0600

    inicialitalizin the list

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Alumno{
-  char nombre[100];
-  int boleta;
-  struct Alumno *sigAlumno;
-}nodoAlumno;
+//Funciones Prototipo
+  //Inicializar la lista
+    void InitList(struct Alumno *sAlumno);
+
+//Definimos el Struct
+  typedef struct Alumno{
+    char nombre[100];
+    int boleta;
+    struct Alumno *sigAlumno;
+  }nodoAlumno;
+
+//Funciones
+  //Función Crear Lista
+  //Funcion que inicialista la lista de structs
+
 
 int main(){
-  
+
 }
